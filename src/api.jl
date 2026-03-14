46db64dea23df8ecac0c2585159b1e345a0011bc
Myeza <34543679+myezanj@users.noreply.github.com>
2026-03-14 16:39:24 +0200

    Update api.jl

--- src/api.jl
+++ src/api.jl
@@ -28,6 +28,12 @@ function file_uri_to_path(uri::AbstractString)::String
     return _string_result(code, out_ptr)
 end
 
+function emptydir(path::AbstractString; create::Bool=true, retries::Integer=0, wait_millis::Integer=0)
+    code = ccall((_c_symbol("emptydir"), _lib()), Cint, (Cstring, Cint, Cint, Cint), path, create ? 1 : 0, retries, wait_millis)
+    _raise_if_error(code)
+    return nothing
+end
+
 function is_ncname(value::AbstractString; strict::Bool=false)::Bool
     out_valid = Ref{Cint}(0)
     code = ccall((_c_symbol("is_ncname"), _lib()), Cint, (Cstring, Cint, Ref{Cint}), value, strict ? 1 : 0, out_valid)
@@ -89,6 +95,205 @@ function map_sql_type_to_tre(sql_type::AbstractString)::Int
     return Int(out_type[])
 end
 
+function get_datasetname(study_name::AbstractString, asset_name::AbstractString,
+    major::Integer, minor::Integer, patch::Integer; include_schema::Bool=true)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall(
+        (_c_symbol("get_datasetname"), _lib()),
+        Cint,
+        (Cstring, Cstring, Cint, Cint, Cint, Cint, Ref{Ptr{UInt8}}),
+        study_name,
+        asset_name,
+        major,
+        minor,
+        patch,
+        include_schema ? 1 : 0,
+        out_ptr,
+    )
+    return _string_result(code, out_ptr)
+end
+
+function get_datafilename(asset_name::AbstractString, major::Integer, minor::Integer, patch::Integer)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall(
+        (_c_symbol("get_datafilename"), _lib()),
+        Cint,
+        (Cstring, Cint, Cint, Cint, Ref{Ptr{UInt8}}),
+        asset_name,
+        major,
+        minor,
+        patch,
+        out_ptr,
+    )
+    return _string_result(code, out_ptr)
+end
+
+function get_datalake_file_path(lake_data::AbstractString, study_name::AbstractString, asset_name::AbstractString,
+    source_file_path::AbstractString, major::Integer, minor::Integer, patch::Integer)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall(
+        (_c_symbol("get_datalake_file_path"), _lib()),
+        Cint,
+        (Cstring, Cstring, Cstring, Cstring, Cint, Cint, Cint, Ref{Ptr{UInt8}}),
+        lake_data,
+        study_name,
+        asset_name,
+        source_file_path,
+        major,
+        minor,
+        patch,
+        out_ptr,
+    )
+    return _string_result(code, out_ptr)
+end
+
+function prepare_datafile(file_path::AbstractString, edam_format::AbstractString;
+    compress::Bool=false, encrypt::Bool=false, precomputed_digest::Union{Nothing, AbstractString}=nothing)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = if precomputed_digest === nothing
+        ccall(
+            (_c_symbol("prepare_datafile_json"), _lib()),
+            Cint,
+            (Cstring, Cstring, Cint, Cint, Cstring, Ref{Ptr{UInt8}}),
+            file_path,
+            edam_format,
+            compress ? 1 : 0,
+            encrypt ? 1 : 0,
+            C_NULL,
+            out_ptr,
+        )
+    else
+        ccall(
+            (_c_symbol("prepare_datafile_json"), _lib()),
+            Cint,
+            (Cstring, Cstring, Cint, Cint, Cstring, Ref{Ptr{UInt8}}),
+            file_path,
+            edam_format,
+            compress ? 1 : 0,
+            encrypt ? 1 : 0,
+            precomputed_digest,
+            out_ptr,
+        )
+    end
+    return _string_result(code, out_ptr)
+end
+
+function dataset_to_arrow(dataset_name::AbstractString, outputdir::AbstractString; replace::Bool=false)
+    out_would_overwrite = Ref{Cint}(0)
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall(
+        (_c_symbol("dataset_to_arrow_output_path"), _lib()),
+        Cint,
+        (Cstring, Cstring, Cint, Ref{Cint}, Ref{Ptr{UInt8}}),
+        dataset_name,
+        outputdir,
+        replace ? 1 : 0,
+        out_would_overwrite,
+        out_ptr,
+    )
+    path = _string_result(code, out_ptr)
+    return (path=path, would_overwrite=(out_would_overwrite[] != 0))
+end
+
+function dataset_to_csv(dataset_name::AbstractString, outputdir::AbstractString; compress::Bool=false, replace::Bool=false)
+    out_would_overwrite = Ref{Cint}(0)
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall(
+        (_c_symbol("dataset_to_csv_output_path"), _lib()),
+        Cint,
+        (Cstring, Cstring, Cint, Cint, Ref{Cint}, Ref{Ptr{UInt8}}),
+        dataset_name,
+        outputdir,
+        compress ? 1 : 0,
+        replace ? 1 : 0,
+        out_would_overwrite,
+        out_ptr,
+    )
+    path = _string_result(code, out_ptr)
+    return (path=path, would_overwrite=(out_would_overwrite[] != 0))
+end
+
+function makeparams(n::Integer)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("makeparams_json"), _lib()), Cint, (Cint, Ref{Ptr{UInt8}}), n, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function quote_ident(name::AbstractString)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("quote_ident"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), name, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function quote_identifier(name::AbstractString, flavour_id::Integer=parse_flavour("postgres"))::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("quote_identifier"), _lib()), Cint, (Cstring, Cint, Ref{Ptr{UInt8}}), name, flavour_id, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function quote_sql_str(value::AbstractString)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("quote_sql_str"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), value, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function julia_type_to_sql_string(julia_type::AbstractString)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("julia_type_to_sql_string"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), julia_type, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function tre_type_to_duckdb_sql(value_type_id::Integer)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("tre_type_to_duckdb_sql"), _lib()), Cint, (Cint, Ref{Ptr{UInt8}}), value_type_id, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function find_system_odbc_driver(driver_name::AbstractString)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("find_system_odbc_driver"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), driver_name, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function find_mssql_driver_in_directory()::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("find_mssql_driver_in_directory"), _lib()), Cint, (Ref{Ptr{UInt8}},), out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function ensure_mssql_driver_registered(driver_name::AbstractString="ODBC Driver 18 for SQL Server")::Bool
+    out_available = Ref{Cint}(0)
+    code = ccall((_c_symbol("ensure_mssql_driver_registered"), _lib()), Cint, (Cstring, Ref{Cint}), driver_name, out_available)
+    _raise_if_error(code)
+    return out_available[] != 0
+end
+
+function _normalize_remote(url::AbstractString)::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("normalize_git_remote"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), url, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
+function git_commit_info(dir::AbstractString="."; short_hash::Bool=true, script_path::AbstractString="")::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall(
+        (_c_symbol("git_commit_info_json"), _lib()),
+        Cint,
+        (Cstring, Cint, Cstring, Ref{Ptr{UInt8}}),
+        dir,
+        short_hash ? 1 : 0,
+        script_path,
+        out_ptr,
+    )
+    return _string_result(code, out_ptr)
+end
+
+function caller_file_runtime(hint_path::AbstractString="")::String
+    out_ptr = Ref{Ptr{UInt8}}(C_NULL)
+    code = ccall((_c_symbol("caller_file_runtime"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), hint_path, out_ptr)
+    return _string_result(code, out_ptr)
+end
+
 function extract_table_from_sql(sql::AbstractString)::String
     out_ptr = Ref{Ptr{UInt8}}(C_NULL)
     code = ccall((_c_symbol("extract_table_from_sql"), _lib()), Cint, (Cstring, Ref{Ptr{UInt8}}), sql, out_ptr)
@@ -111,9 +316,9 @@ function map_redcap_value_type(field_type::AbstractString, validation::Union{Not
     out_type = Ref{Cint}(0)
     out_fmt = Ref{Ptr{UInt8}}(C_NULL)
     code = if validation === nothing
-        ccall((_c_symbol("map_redcap_value_type"), _lib()), Cint, (Cstring, Cstring, Ref{Cint}, Ref{Ptr{UInt8}}), field_type, C_NULL, out_type, out_fmt)
+        ccall((_c_symbol("map_value_type"), _lib()), Cint, (Cstring, Cstring, Ref{Cint}, Ref{Ptr{UInt8}}), field_type, C_NULL, out_type, out_fmt)
     else
-        ccall((_c_symbol("map_redcap_value_type"), _lib()), Cint, (Cstring, Cstring, Ref{Cint}, Ref{Ptr{UInt8}}), field_type, validation, out_type, out_fmt)
+        ccall((_c_symbol("map_value_type"), _lib()), Cint, (Cstring, Cstring, Ref{Cint}, Ref{Ptr{UInt8}}), field_type, validation, out_type, out_fmt)
     end
     _raise_if_error(code)
 
